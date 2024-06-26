2ff163c9a706041383ed252115d989445996404f
serafimiumroadtojunior <167617840+serafimiumroadtojunior@users.noreply.github.com>
2024-06-26 13:13:09 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,24 +1,25 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <QMainWindow>  // Включаем основной класс главного окна QT
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Diskriminant; }
+namespace Ui { class Diskriminant; }  // Объявляем пространство имен Ui с классом Diskriminant
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow {
-    Q_OBJECT
+class MainWindow : public QMainWindow {  // Определяем класс MainWindow, который наследуется от QMainWindow
+    Q_OBJECT  // Макрос Q_OBJECT для поддержки метаобъектной системы Qt
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    MainWindow(QWidget *parent = nullptr);  // Конструктор класса MainWindow, принимающий указатель на родительский виджет
+    ~MainWindow();  // Деструктор класса MainWindow
 
 private slots:
-    void on_calculateButton_clicked();
+    void on_calculateButton_clicked();  // Приватный слот для обработки нажатия кнопки "calculateButton"
 
 private:
-    Ui::Diskriminant *ui;
+    Ui::Diskriminant *ui;  // Указатель на объект класса Ui::Diskriminant для работы с интерфейсом
 };
 
-#endif // MAINWINDOW_H
+#endif // MAINWINDOW_H  // Завершаем условную компиляцию директивой #endif
+
