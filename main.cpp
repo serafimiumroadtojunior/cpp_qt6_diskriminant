e406691becf92d3ddb329bac02644d7987c81ef7
serafimiumroadtojunior <167617840+serafimiumroadtojunior@users.noreply.github.com>
2024-06-26 11:36:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <QApplication>
-#include "mainwindow.h"
+#include <QApplication>   // Подключаем файл QApplication который обеспечивает функционал
+#include "mainwindow.h"  // Подключаем файл нашего главного окна MainWindow
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    MainWindow window;
-    window.show();
-    return app.exec();
+    QApplication app(argc, argv);  // Создаем объект приложения QApplication
+    MainWindow window;  // Создаем объект класса окно MainWindow
+    window.show();  // отображаем окно MainWindow 
+    return app.exec();  // Запускаем цикл обработки событий приложения и возвращаем код после завершения
 }
